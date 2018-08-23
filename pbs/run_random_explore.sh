67c125ee8ecb078033631dfe1f94978cbbcb94cc
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2018-08-23 23:02:40 +0200

    Create bash script to run all simulations in screens

---
+++ pbs/run_random_explore.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+screen -d -m -S crem_o_r1 bash -c 'python ~/python/crem/pbs/random_explore.py -o ~/imtm/crem/gen/orgelm_r1_f0 -d ~/imtm/crem/db/orgelm/replacements.db -r 1'
+screen -d -m -S crem_o_r2 bash -c 'python ~/python/crem/pbs/random_explore.py -o ~/imtm/crem/gen/orgelm_r2_f0 -d ~/imtm/crem/db/orgelm/replacements.db -r 2'
+screen -d -m -S crem_o_r3 bash -c 'python ~/python/crem/pbs/random_explore.py -o ~/imtm/crem/gen/orgelm_r3_f0 -d ~/imtm/crem/db/orgelm/replacements.db -r 3'
+screen -d -m -S crem_pains_r1 bash -c 'python ~/python/crem/pbs/random_explore.py -o ~/imtm/crem/gen/orgelm_pains_r1_f0 -d ~/imtm/crem/db/orgelm_pains/replacements.db -r 1'
+screen -d -m -S crem_pains_r2 bash -c 'python ~/python/crem/pbs/random_explore.py -o ~/imtm/crem/gen/orgelm_pains_r2_f0 -d ~/imtm/crem/db/orgelm_pains/replacements.db -r 2'
+screen -d -m -S crem_pains_r3 bash -c 'python ~/python/crem/pbs/random_explore.py -o ~/imtm/crem/gen/orgelm_pains_r3_f0 -d ~/imtm/crem/db/orgelm_pains/replacements.db -r 3'
+screen -d -m -S crem_pains_r4 bash -c 'python ~/python/crem/pbs/random_explore.py -o ~/imtm/crem/gen/orgelm_pains_r4_f0 -d ~/imtm/crem/db/orgelm_pains/replacements.db -r 4'
+screen -d -m -S crem_pains_r5 bash -c 'python ~/python/crem/pbs/random_explore.py -o ~/imtm/crem/gen/orgelm_pains_r5_f0 -d ~/imtm/crem/db/orgelm_pains/replacements.db -r 5'

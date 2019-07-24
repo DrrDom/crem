fd59fa3879350a1a37c4a76eeff60ff316435ece
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2019-07-24 11:16:44 +0200

    Make code and algorithm clear, add three levels of patience

--- crem_gua3.py
+++ crem_gua3.py
@@ -20,6 +20,7 @@ from typing import List, Optional
 
 import joblib
 import numpy as np
+import pandas as pd
 
 import sys
 sys.path.append('/home/pavlop/python/guacamol')
@@ -81,7 +82,7 @@ class CREM_Generator(GoalDirectedGenerator):
                  generations, ncpu, random_start, output_dir):
         self.pool = joblib.Parallel(n_jobs=ncpu)
         self.smiles = self.load_smiles_from_file(smi_file)
-        self.selection_size = selection_size
+        self.N = selection_size
         self.radius = radius
         self.min_size = min_size
         self.max_size = max_size
@@ -91,9 +92,9 @@ class CREM_Generator(GoalDirectedGenerator):
         self.replacements_baseline = replacements
         self.generations = generations
         self.random_start = random_start
-        self.patience = 2
+        self.patience1 = 3
         self.patience2 = 10
-        self.patience_restart = 24
+        self.patience3 = 33
         self.task = 0
         self.output_dir = output_dir
 
@@ -109,7 +110,8 @@ class CREM_Generator(GoalDirectedGenerator):
         scored_smiles = sorted(scored_smiles, key=lambda x: x[0], reverse=True)
         return [smile for score, smile in scored_smiles][:k]
 
-    def generate(self, mols):
+    def generate(self, smiles):
+        mols = [Chem.AddHs(Chem.MolFromSmiles(s)) for s in smiles]
         res = self.pool(delayed(mutate_mol2)(mol, db_name=db_fname,
                                              radius=self.radius, min_size=self.min_size,
                                              max_size=self.max_size,
@@ -123,162 +125,146 @@ class CREM_Generator(GoalDirectedGenerator):
         res = set(m[0] for sublist in res for m in sublist)
         return list(res)
 
-    def get_params(self, score):
+    def set_params(self, score):
         # get min_inc, max_inc, max_replacements
+        self.replacements = self.replacements_baseline
         if score > 0.8:
-            return -4, 4, self.replacements_baseline + 1000
-        if score > 0.6:
-            return -5, 5, self.replacements_baseline + 200
-        if score > 0.4:
-            return -6, 6, self.replacements_baseline + 100
-        if score > 0.3:
-            return -7, 7, self.replacements_baseline
-        if score > 0.2:
-            return -8, 8, self.replacements_baseline
-        return -9, 9, self.replacements_baseline
+            self.min_inc = -4
+            self.max_inc = 4
+        elif score > 0.7:
+            self.min_inc = -5
+            self.max_inc = 5
+        elif score > 0.6:
+            self.min_inc = -6
+            self.max_inc = 6
+        elif score > 0.5:
+            self.min_inc = -7
+            self.max_inc = 7
+        elif score > 0.4:
+            self.min_inc = -8
+            self.max_inc = 8
+        elif score > 0.3:
+            self.min_inc = -9
+            self.max_inc = 9
+        else:
+            self.min_inc = -10
+            self.max_inc = 10
+
+    def get_scores(self, scoring_function, smiles):
+        mols = [Chem.MolFromSmiles(s) for s in smiles]
+        return self.pool(delayed(score_mol)(m, scoring_function.score) for m in mols)
 
     def generate_optimized_molecules(self, scoring_function: ScoringFunction, number_molecules: int,
                                      starting_population: Optional[List[str]] = None) -> List[str]:
 
         self.task += 1
-        restart = starting_population is None
 
-        if number_molecules > self.selection_size:
-            self.selection_size = number_molecules
+        if number_molecules > self.N:
+            self.N = number_molecules
             print(f'Benchmark requested more molecules than expected: new population is {number_molecules}')
 
+        # select initial population
         if starting_population is None:
             print('selecting initial population...')
             if self.random_start:
-                starting_population = np.random.choice(self.smiles, self.selection_size)
+                population = pd.DataFrame(np.random.choice(self.smiles, self.N), columns=['smi'])
             else:
-                starting_population = self.top_k(self.smiles, scoring_function, self.selection_size)
-
-        # select initial population
-        population_smiles = heapq.nlargest(self.selection_size, starting_population, key=scoring_function.score)
-        population_mols = [Chem.MolFromSmiles(s) for s in population_smiles]
-        population_scores = self.pool(delayed(score_mol)(m, scoring_function.score) for m in population_mols)
+                population = pd.DataFrame(self.top_k(self.smiles, scoring_function, self.N), columns=['smi'])
+        else:
+            population = pd.DataFrame(starting_population, columns=['smi'])
+        population['score'] = self.get_scores(scoring_function, population['smi'])
 
         # evolution: go go go!!
         t0 = time()
 
-        patience = 0
+        patience1 = 0
         patience2 = 0
-        patience2_score = 0
-        patience_restart = 0
+        patience3 = 0
 
-        used_smiles = set(population_smiles)   # smiles already used for mutation
-        best_smiles = set()
-        ref_score = max(population_scores)
-
-        self.min_inc, self.max_inc, self.replacements = self.get_params(max(population_scores))
+        best = population.copy().drop_duplicates(subset='smi')
+        ref_score = np.mean(best['score'].iloc[:number_molecules])
+        self.set_params(max(best['score']))
+        used_smiles = set(population['smi'])   # smiles already used for mutation
 
         for generation in range(self.generations):
 
-            new_smiles = set(self.generate(population_mols))
-            new_mols = [Chem.MolFromSmiles(s) for s in new_smiles]
-            new_scores = self.pool(delayed(score_mol)(m, scoring_function.score) for m in new_mols)
+            if ref_score == 1:
+                break
 
-            best_smiles = list(best_smiles) + list(zip(new_scores, new_smiles))
-            best_smiles = set(best_smiles)
-            best_smiles = sorted(best_smiles, key=lambda x: x[0], reverse=True)[:number_molecules]
+            population = pd.DataFrame(list(set(self.generate(population['smi']))), columns=['smi'])
+            population['score'] = self.get_scores(scoring_function, population['smi'])
+            population.sort_values(by='score', ascending=False, inplace=True)
+            population.drop_duplicates(subset='smi', inplace=True)
 
-            # update parameters
-            if max(new_scores) <= ref_score and max(new_scores) < 1:
-                if patience >= self.patience:
-                    self.min_inc -= 1
-                    self.max_inc += 1
-                    patience = 0
-                    used_smiles = set()
-                    ref_score = max(new_scores)
-                else:
-                    patience += 1
-            else:
-                patience = 0
-                ref_score = max(new_scores)
-                self.min_inc, self.max_inc, self.replacements = self.get_params(best_smiles[0][0])
+            best = best.append(population).\
+                drop_duplicates(subset='smi').\
+                sort_values(by='score', ascending=False).\
+                head(self.N)
+            cur_score = np.mean(best['score'].iloc[:number_molecules])
 
-            if sum(score for score, smi in best_smiles) <= patience2_score:
-                patience_restart += 1
-                if patience2 >= self.patience2:
-                    self.min_inc -= 15
-                    self.max_inc += 15
-                    self.replacements = 10000
-                    patience2 = 0
-                    patience = -5
-                else:
-                    patience2 += 1
-            else:
-                patience2_score = sum(score for score, smi in best_smiles)
+            if cur_score > ref_score:
+                ref_score = cur_score
+                population = population.head(self.N)
+                self.set_params(max(population['score']))
+                used_smiles.update(population['smi'])
+                patience1 = 0
                 patience2 = 0
-                patience_restart = 0
-
-            # select next population
-            new_tuples = []
-            for score, mol, smi in zip(new_scores, new_mols, new_smiles):
-                if smi not in population_smiles:
-                    new_tuples.append((score, mol, smi))
-            new_tuples += list(zip(population_scores, population_mols, population_smiles))
-            new_tuples = sorted(new_tuples, key=lambda x: x[0], reverse=True)
-
-            ids = []
-            for i, (score, mol, smi) in enumerate(new_tuples):
-                if smi not in used_smiles:
-                    ids.append(i)
-                    if len(ids) == self.selection_size:
+                patience3 = 0
+            else:
+                patience1 += 1
+                patience2 += 1
+                patience3 += 1
+                if patience3 >= self.patience3:
+                    if starting_population is None and self.random_start:
+                        patience1 = 0
+                        patience2 = 0
+                        patience3 = 0
+                        population = pd.DataFrame(np.random.choice(self.smiles, self.N), columns=['smi']).drop_duplicates(subset='smi')
+                        population['score'] = self.get_scores(scoring_function, population['smi'])
+                        population.sort_values(by='score', ascending=False, inplace=True)
+                        self.set_params(max(population['score']))
+                        used_smiles = set(population['smi'])
+                    else:
                         break
-                    
-            #ids = select_new_pool(ids, [new_tuples[i][0] for i in ids], self.selection_size)
-
-            population_scores = [new_tuples[i][0] for i in ids]
-            population_mols = [new_tuples[i][1] for i in ids]
-            population_smiles = [new_tuples[i][2] for i in ids]
-
-            used_smiles.update(population_smiles)
+                else:
+                    population = population.head(self.N)
+                    used_smiles.update(population['smi'])
+                    if patience2 >= self.patience2:
+                        patience1 = 0
+                        patience2 = 0
+                        self.min_inc -= 10
+                        self.max_inc += 10
+                        self.replacements += 500
+                        used_smiles = set(population['smi'])
+                    elif patience1 >= self.patience1:
+                        patience1 = 0
+                        self.min_inc -= 1
+                        self.max_inc += 1
+                        self.replacements += 100
+                        used_smiles = set(population['smi'])
 
             # stats
             gen_time = time() - t0
             t0 = time()
-
             print(f'{generation: >5} | '
-                  f'best avg: {round(sum(score for score, smi in best_smiles) / len(best_smiles), 3)} | '
-                  f'max: {np.max(population_scores):.3f} | '
-                  f'avg: {np.mean(population_scores):.3f} | '
-                  f'min: {np.min(population_scores):.3f} | '
-                  f'std: {np.std(population_scores):.3f} | '
-                  f'sum: {np.sum(population_scores):.3f} | '
+                  f'best avg: {np.round(np.mean(best["score"].iloc[:number_molecules]), 3)} | '
+                  f'max: {np.max(population["score"]):.3f} | '
+                  f'avg: {np.mean(population["score"]):.3f} | '
+                  f'min: {np.min(population["score"]):.3f} | '
+                  f'std: {np.std(population["score"]):.3f} | '
+                  f'sum: {np.sum(population["score"]):.3f} | '
                   f'min_inc: {self.min_inc} | '
                   f'max_inc: {self.max_inc} | '
-                  f'replacements: {self.replacements} | '
-                  f'patience: {patience} | '
-                  f'patience2: {patience2} | '
+                  f'repl: {self.replacements} | '
+                  f'p1: {patience1} | '
+                  f'p2: {patience2} | '
+                  f'p3: {patience3} | '
                   f'{gen_time:.2f} sec')
-
             sys.stdout.flush()
 
-            # everything is perfect
-            if all(score == 1 for score, smi in best_smiles):
-                break
-
-            # optimization get stuck - no improvement for a long time
-            if patience_restart >= self.patience_restart:
-                if restart:
-                    # restart optimization from another random seeds
-                    starting_population = np.random.choice(self.smiles, self.selection_size)
-                    population_smiles = heapq.nlargest(self.selection_size, starting_population, key=scoring_function.score)
-                    population_mols = [Chem.MolFromSmiles(s) for s in population_smiles]
-                    population_scores = self.pool(delayed(score_mol)(m, scoring_function.score) for m in population_mols)
-                    ref_score = max(population_scores)
-                    patience_restart = 0
-                else:
-                    # give up
-                    break
-
         # finally
-        with open(os.path.join(self.output_dir, f'{self.task}.smi'), 'wt') as f:
-            for score, smi in best_smiles:
-                f.write(f'{smi}\t{round(score, 3)}\n')
-        return [smi for score, smi in best_smiles]
+        best.round({'score': 3}).to_csv(os.path.join(self.output_dir, f'{self.task}.smi'), sep="\t", header=False, index=False)
+        return best['smi'][:number_molecules]
 
 
 def main():

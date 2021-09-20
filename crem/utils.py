d00bf331a5e392a8638bf26b4f49ef436ce39a3a
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2021-09-20 13:42:27 +0200

    Fix several errors and reformat docstrings

--- crem/utils.py
+++ crem/utils.py
@@ -1,5 +1,6 @@
 import sys
 from collections import OrderedDict
+from multiprocessing import cpu_count
 
 import joblib
 
@@ -8,6 +9,7 @@ from .crem import grow_mol2, mutate_mol2
 
 def __get_child_added_atom_ids(child_mol):
     '''
+    Returns ids of atoms in a current mol which were added upon grow/mutation procedure
     # After RDKit reaction procedure there is a field <react_atom_idx> with initial parent atom idx in a child mol
     '''
     added_mol_ids = []
@@ -21,7 +23,8 @@ def __get_child_protected_atom_ids(mol, protected_parent_ids):
     '''
 
     :param mol:
-    :param protected_parent_ids: list[int]
+    :param protected_parent_ids: ids of a parent molecule which were protected and should be transferred to the
+                                 current molecule
     :type  protected_parent_ids: list[int]
     :return: sorted list of integers
     '''
@@ -40,62 +43,55 @@ def __mol_with_atom_index(mol):
     return mol
 
 
-def enumerate_compounds(mol, db_fname, num_iteration, mode='scaffold', radius=3, max_replacements=None,
-                        min_freq=0, protected_ids=None, replase_ids=None, ncpu=1, protect_added_frag=False, **kwargs):
+def enumerate_compounds(mol, db_fname, mode='scaffold', n_iterations=1, radius=3, max_replacements=None,
+                        min_freq=0, protected_ids=None, replace_ids=None, protect_added_frag=False, ncpu=None,
+                        **kwargs):
     '''
+    Convenience function to perform scaffold decoration or enumeration of analog series. This performs in multiple
+    iterations by modification of compounds enumerated on the previous iteration. May result in combinatorial explosion.
+
+    Scaffold decoration uses grow procedure. Hydrogens will be added to the supplied molecule and replaced with
+    fragments from the database. A user can protect particular positions from expansion by setting protect_ids argument.
+    Note: The value of protect_added_frag parameter is ignored. New fragments cannot be attached to previously
+    added fragments.
+
+    Enumeration of analog series uses mutate procedure. The molecule should be supplied with explicit hydrogens if one
+    wants to replace them as well, otherwise only heavy atoms will be considered for replacements. It is recommended to
+    set an altered part of a molecule not too large to obtain reasonable suggestions.
+
     :param mol:
     :param db_fname: path to DB file with fragment replacements.
-    :param num_iteration: int. The number of rounds of substitution
-    :param protected_ids: iterable with atom ids which will not be growed/mutated. If the molecule was supplied with explicit
-                          hydrogen the ids of protected hydrogens should be supplied as well, otherwise they will be
-                          replaced. Default: None.
-    :param replased_ids: iterable with atom ids to replace, it has lower priority over `protected_ids` (replace_ids
-                        which are present in protected_ids would be protected).
-                        Ids of hydrogen atoms (if any) connected to the specified heavy atoms will be automatically
-                        labeled as replaceable. Default: None.
-    :param ncpu: number of cores
-    :param mode: scaffold or analogs
-    *scaffold
-    To decorate user-chosen scaffold.
-    Scaffold decoration uses grow procedure. Set user-defined atoms to grow/protect.
-    Hydrogens will be added and replaced with fragments from the database.
-    Note: The value of protect_added_frag parameter is ignored. New fragments cannot be attached to added fragments.
-
-    *analogs
-    To enumerate analog series.
-    Enumeration of analog series can use grow/mutate procedures.
-    There are possible next variants:
-    1) To replace only hydrogens and grow molecule set min_size and max_size parameters as 0.
-    2) To mutate non-hydrogens not-protected atoms of molecule set min_size and max_size more than 0
-    3) To mutate all not-protected atoms of molecule (include hydrogens) set min_size as 0 (Default: 0) and max_size more than 0 (Default: 10).
-    Note: Hydrogens should be added by user before running this option. Hydrogens ids should be numerate in protected_ids or replase_ids as well.
-
-    :param protect_added_frag: True or False. If set True new fragments cannot be attached to previously added fragments.
-    For scaffold mode protect_added_frag arguments set True and user set will be ignored. Default: False
+    :param mode: 'scaffold' decoration or 'analogs' enumeration. In 'scaffold' mode the supplied molecule will be
+                 substituted with fragments from a database. In 'analogs' mode the supplied molecule wil be mutated.
+                 Default: scaffold.
+    :param n_iterations: the number of rounds of generation. Molecules generated on the previous round are supplied
+                         to the next iteration. Be careful setting this parameter as it may cause combinatorial
+                         explosion. Default: 1.
+    :param radius: radius of context which will be considered for replacement. Default: 3.
+    :param max_replacements: maximum number of replacements to make for each molecule on each iteration. None will
+                             result in all possible replacements or it is possible to set a randomly chosen number of
+                             replacements. Default: None.
+    :param protected_ids: iterable with atom ids which will not be altered. In 'scaffold' mode this can be ids of
+                          hydrogens or heavy atoms whose hydrogens should be protected from expansion. In 'analogs' mode
+                          these are ids of hydrogens and heavy atoms. Please note, a molecule should be supplied with
+                          explicit hydrogens in 'analogs' mode if one wants to replace them. Default: None.
+    :param replace_ids: iterable with atom ids to replace, it has lower priority then `protected_ids`. Default: None.
+    :param protect_added_frag: True or False. If set True new fragments cannot be attached/replace fragments added on
+                               previous iterations. Applicable only in 'analogs' mode. In 'scaffold' mode user input is
+                               ignored and the argument internally set to True. Default: False
+    :param ncpu: number of cores. None means all cpus.
 
     *Common grow/mutate arguments*
-    :param radius: radius of context which will be considered for replacement. Default: 3.
-    :param max_replacements: maximum number of replacements to make. If the number of replacements available in DB is
-                             greater than the specified value the specified number of randomly chosen replacements
-                             will be applied. Default: None.
     :param min_freq: minimum occurrence of fragments in DB for replacement. Default: 0.
-    :param protected_ids: iterable with atom ids which will not be mutated. If the molecule was supplied with explicit
-                          hydrogen the ids of protected hydrogens should be supplied as well, otherwise they will be
-                          replaced.
-                          Ids of all equivalent atoms should be supplied (e.g. to protect meta-position in toluene
-                          ids of both carbons in meta-positions should be supplied)
-                          This argument has a higher priority over `replace_ids`. Default: None.
-    :param replace_ids: iterable with atom ids to replace, it has lower priority over `protected_ids` (replace_ids
-                        which are present in protected_ids would be protected).
-                        Ids of hydrogen atoms (if any) connected to the specified heavy atoms will be automatically
-                        labeled as replaceable. Default: None.
 
-    :param kwargs:
-    *grow arguments*
-    :min_atoms: minimum number of atoms in the fragment which will replace H
-    :max_atoms: maximum number of atoms in the fragment which will replace H
+    :param kwargs: these arguments will be passed to grow_mol (for 'scaffold' mode) and mutate_mol ('analogs' mode)
+    functions.
 
-    *mutate arguments*
+    arguments relevant to 'scaffold' mode
+    min_atoms: minimum number of atoms in the fragment which will replace H
+    max_atoms: maximum number of atoms in the fragment which will replace H
+
+    arguments relevant to 'analogs' mode
     :min_size: minimum number of heavy atoms in a fragment to replace. If 0 - hydrogens will be replaced
                      (if they are explicit). Default: 0.
     :max_size: maximum number of heavy atoms in a fragment to replace. Default: 10.
@@ -109,11 +105,15 @@ def enumerate_compounds(mol, db_fname, num_iteration, mode='scaffold', radius=3,
 
     '''
 
-    pool = joblib.Parallel(n_jobs=ncpu)
-
     if mode not in ['scaffold', 'analogs']:
         print('Wrong mode. Please choose one from the list - "analogs","scaffold"')
         return None
+
+    if ncpu is None:
+        ncpu = cpu_count()
+    else:
+        ncpu = max(1, min(int(ncpu), cpu_count()))
+    pool = joblib.Parallel(n_jobs=ncpu)
 
     # to check if the statical arguments are in the kwargs dict
     for kw in ['return_mol', 'return_rxn', 'return_rxn_freq', 'ncores']:
@@ -123,28 +123,30 @@ def enumerate_compounds(mol, db_fname, num_iteration, mode='scaffold', radius=3,
     if mode == 'scaffold':
         protect_added_frag = True
 
-    if protected_ids is None and replase_ids is not None:
-        protected_ids = list(set(a.GetIdx() for a in mol.GetAtoms()).difference(replase_ids))
+    if protected_ids is None and replace_ids is not None:
+        protected_ids = list(set(a.GetIdx() for a in mol.GetAtoms()).difference(replace_ids))
+    if protected_ids is None:
+        protected_ids = []
 
     start_mols = {mol: protected_ids}
     # to get results ordered by iterations
     generated_mols = OrderedDict()
     n = 0
 
-    for n in range(num_iteration):
+    for n in range(n_iterations):
         new_mols = ()
         if mode == 'scaffold':
             new_mols = pool(joblib.delayed(grow_mol2)(m, db_name=db_fname, protected_ids=prot_ids,
                                                       min_freq=min_freq, radius=radius,
                                                       max_replacements=max_replacements,
                                                       return_mol=True, return_rxn=False, return_rxn_freq=False,
-                                                      ncores=1 if len(start_mols) != 1 else ncpu, **kwargs)
+                                                      ncores=1 if len(start_mols) > 1 else ncpu, **kwargs)
                             for m, prot_ids in start_mols.items())
         if mode == 'analogs':
             new_mols = pool(joblib.delayed(mutate_mol2)(m, db_name=db_fname, protected_ids=prot_ids,
                                                         min_freq=0, radius=radius, max_replacements=max_replacements,
                                                         return_mol=True, return_rxn=False, return_rxn_freq=False,
-                                                        ncores=1 if len(start_mols) != 1 else ncpu, **kwargs)
+                                                        ncores=1 if len(start_mols) > 1 else ncpu, **kwargs)
                             for m, prot_ids in start_mols.items())
 
         parent_protected_ids_list = start_mols.values()
@@ -157,12 +159,13 @@ def enumerate_compounds(mol, db_fname, num_iteration, mode='scaffold', radius=3,
                         protect_added_ids = __get_child_added_atom_ids(items[1])
                         protected_ids = set(protected_ids + protect_added_ids)
 
-                        generated_mols[items[0]] = items[1]
-                        start_mols[items[1]] = protected_ids
+                    generated_mols[items[0]] = items[1]
+                    start_mols[items[1]] = protected_ids
 
         if not start_mols:
             break
 
-    sys.stderr.write(f'Procedure is finished.\n{n + 1} iterations were completed successfully.\n'
-                     f'Totally {len(generated_mols)} new compounds were generated.\n')
+    if n + 1 < n_iterations:
+        sys.stderr.write(f'INFO. Procedure is finished after {n + 1} iterations instead of {n_iterations}\n')
+
     return generated_mols

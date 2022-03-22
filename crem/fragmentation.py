6ddfba45ab904f1a4b53610b7d38dd96fd82a1e5
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2022-03-22 14:33:19 +0100

    Add modes to fragmentation.py to process only heavy atoms/hydrogens and tolerate empty lines in input

--- crem/fragmentation.py
+++ crem/fragmentation.py
@@ -8,7 +8,7 @@ from rdkit import Chem
 from rdkit.Chem import rdMMPA
 
 
-def fragment_mol(smi, smi_id=''):
+def fragment_mol(smi, smi_id='', mode=0):
 
     mol = Chem.MolFromSmiles(smi)
 
@@ -18,32 +18,37 @@ def fragment_mol(smi, smi_id=''):
         sys.stderr.write("Can't generate mol for: %s\n" % smi)
     else:
         # heavy atoms
-        frags = rdMMPA.FragmentMol(mol, pattern="[!#1]!@!=!#[!#1]", maxCuts=4, resultsAsMols=False, maxCutBonds=30)
-        frags += rdMMPA.FragmentMol(mol, pattern="[!#1]!@!=!#[!#1]", maxCuts=3, resultsAsMols=False, maxCutBonds=30)
-        frags = set(frags)
-        for core, chains in frags:
-            output = '%s,%s,%s,%s\n' % (smi, smi_id, core, chains)
-            outlines.add(output)
-        # hydrogen splitting
-        mol = Chem.AddHs(mol)
-        n = mol.GetNumAtoms() - mol.GetNumHeavyAtoms()
-        if n < 60:
-            frags = rdMMPA.FragmentMol(mol, pattern="[#1]!@!=!#[!#1]", maxCuts=1, resultsAsMols=False, maxCutBonds=100)
+        if mode == 0 or mode == 1:
+            frags = rdMMPA.FragmentMol(mol, pattern="[!#1]!@!=!#[!#1]", maxCuts=4, resultsAsMols=False, maxCutBonds=30)
+            frags += rdMMPA.FragmentMol(mol, pattern="[!#1]!@!=!#[!#1]", maxCuts=3, resultsAsMols=False, maxCutBonds=30)
+            frags = set(frags)
             for core, chains in frags:
                 output = '%s,%s,%s,%s\n' % (smi, smi_id, core, chains)
                 outlines.add(output)
+        # hydrogen splitting
+        if mode == 1 or mode == 2:
+            mol = Chem.AddHs(mol)
+            n = mol.GetNumAtoms() - mol.GetNumHeavyAtoms()
+            if n < 60:
+                frags = rdMMPA.FragmentMol(mol, pattern="[#1]!@!=!#[!#1]", maxCuts=1, resultsAsMols=False, maxCutBonds=100)
+                for core, chains in frags:
+                    output = '%s,%s,%s,%s\n' % (smi, smi_id, core, chains)
+                    outlines.add(output)
     return outlines
 
 
-def process_line(line, sep):
+def process_line(line, sep, mode):
     tmp = line.strip().split(sep)
-    if len(tmp) == 1:
-        return fragment_mol(tmp[0])
+    if tmp:
+        if len(tmp) == 1:
+            return fragment_mol(tmp[0], mode=mode)
+        else:
+            return fragment_mol(tmp[0], tmp[1], mode=mode)
     else:
-        return fragment_mol(tmp[0], tmp[1])
+        return None
 
 
-def main(input_fname, output_fname, sep, ncpu, verbose):
+def main(input_fname, output_fname, mode, sep, ncpu, verbose):
 
     ncpu = min(cpu_count(), max(ncpu, 1))
     p = Pool(ncpu)
@@ -52,9 +57,10 @@ def main(input_fname, output_fname, sep, ncpu, verbose):
 
         with open(input_fname) as f:
 
-            for i, res in enumerate(p.imap_unordered(partial(process_line, sep=sep), f, chunksize=100), 1):
+            for i, res in enumerate(p.imap_unordered(partial(process_line, sep=sep, mode=mode), f, chunksize=100), 1):
 
-                out.write(''.join(res))
+                if res:
+                    out.write(''.join(res))
 
                 if verbose and i % 1000 == 0:
                     sys.stderr.write('\r%i molecules fragmented' % i)
@@ -71,6 +77,9 @@ def entry_point():
                         help='fragmented molecules.')
     parser.add_argument('-s', '--sep', metavar='STRING', required=False, default=None,
                         help='separator in input file. Default: Tab.')
+    parser.add_argument('-m', '--mode', metavar='INTEGER', required=False, default=0, choices=[0, 1, 2], type=int,
+                        help='fragmentation mode: 0 - all atoms constitute a fragment, 1- heavy atoms only, '
+                             '2 - hydrogen atoms only. Default: 0.')
     parser.add_argument('-c', '--ncpu', metavar='NUMBER', required=False, default=1,
                         help='number of cpus used for computation. Default: 1.')
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
@@ -83,10 +92,12 @@ def entry_point():
         if o == "verbose": verbose = v
         if o == "ncpu": ncpu = int(v)
         if o == "sep": sep = v
+        if o == "mode": mode = v
 
     main(input_fname=input_fname,
          output_fname=output_fname,
          sep=sep,
+         mode=mode,
          ncpu=ncpu,
          verbose=verbose)
 

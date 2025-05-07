f5195a6738625cf2d2c29591e5485dba22126b76
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2025-05-07 14:35:23 +0200

    Add MANIFEST.in adn update setup.py to include scripts dir

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/DrrDom/crem",
     packages=['crem'],
+    include_package_data=True,
+    package_data={'crem': ['scripts/*']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
